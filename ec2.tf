5828a95e5ee93c5422d6ae6cedecb8f23f57947c
Satya kama <satyakama@Satyas-MacBook-Pro.local>
2025-05-07 03:28:59 +0530

    added instances and security groups.

--- ec2.tf
+++ ec2.tf
@@ -34,10 +34,63 @@ resource "aws_subnet" "private_2" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  subnet_id = aws_subnet.private.id
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
 
   tags = {
     Name = "Kafka_testing"
   }
+}
+
+resource "aws_instance" "web-1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.private_1.id
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "Kafka_test-2"
+  }
+}
+
+resource "aws_instance" "web-2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.private_2.id
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+
+  tags = {
+    Name = "Kafka_test-3"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_nat_gateway" "NAT-1" {
@@ -61,7 +114,7 @@ resource "aws_route_table" "igw_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
@@ -74,7 +127,7 @@ resource "aws_route_table" "nat_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.NAT-1.id
   }
 
@@ -89,6 +142,11 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "IGW_VPC"
   }
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.test_role.name
 }
 
 resource "aws_iam_role" "test_role" {
